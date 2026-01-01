5a7494e6de4c18b1e9ba822ac5504f70c24d6664
Prachi Patil <prachipatil0923@gmail.com>
2026-01-01 22:12:57 +0530

    Create Welcome2026.java
    Added Welcome 2026 Java program

---
+++ Welcome2026.java
@@ -0,0 +1,7 @@
+public class Welcome2026 {
+    public static void main(String[] args) {
+        System.out.println("Welcome 2026!");
+        System.out.println("New Year, New Goals, New Code.");
+        System.out.println("Consistency is the key to success.");
+    }
+}
